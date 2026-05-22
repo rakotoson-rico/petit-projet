ede769615e63bc0595949bbf96b8c0399b549496
rakotoson-rico <rakotosonricost@gmail.com>
2026-05-22 20:58:11 +0300

    Équations 2nd degrés

---
+++ equation.py
@@ -0,0 +1,16 @@
+a=int(input("a= "))
+b=int(input("b= "))
+c=int(input("c= "))
+
+D=int(b*b + (4*a*c))
+print(D)
+if D<0 :
+    print("solution vide")
+    if D>0 :
+        import math
+        x1=(-b+ sqrt(D))/2*a
+        print(x1)
+    if D :=O :
+        x1=x2=(-b/2*a)
+        print(x1)
+
